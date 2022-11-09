9f43c5811d7c8187805746634c85cec8ac59a269
KryptonANSHU <anshu191001@gmail.com>
2022-11-09 22:55:28 +0530

    Adding Tab View

---
+++ App.js
@@ -0,0 +1,18 @@
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import InshortTab from './components/InshortTab';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <InshortTab />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   marginTop : StatusBar.currentHeight,
+  },
+});
